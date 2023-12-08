290d969f9f012e53f8804860a917aa77cac6f8dc
Prateek Lohani <67368888+Prateek-Lohani@users.noreply.github.com>
2023-12-09 01:15:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+gsap.from('.page1',
+{
+  x:-1000,
+  duration:2,
+  scrollTrigger:{
+    scroller:'body',
+    trigger:'.page1',
+    pin:true
+  }
+})
+
+gsap.from('.page2 h3',{
+  x:0,
+  opacity:0,
+  duration:3,
+  scrollTrigger:{
+    scroller:'body',
+    trigger:'.page2',
+    scrub:2,
+    pin:true
+  }
+})
+
+gsap.from('.page3',{
+  scale:0,
+  duration:0.5,
+  scrollTrigger:{
+    markers:true,
+    scroller:'body',
+    trigger:'.page3',
+    start:'top 240%',
+    end:'top 70%',
+    scrub:1
+  }
+})
